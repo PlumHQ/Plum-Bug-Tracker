ecf1038ec7805de8692ca4cb62f9e14e470b080c
kartikBhardwajPlum <kartik.b@plumhq.com>
2024-08-28 10:51:51 +0530

    init

---
+++ config_inc_addon.php
@@ -0,0 +1,12 @@
+<?php
+
+$g_webmaster_email          = "kartik.b@plumhq.com";
+$g_from_email          =  "bugtracker@plumhq.com";
+$g_return_path_email          = "kartik.b@plumhq.com";
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'smtp.postmarkapp.com';
+$g_smtp_username = 'serverkeyhere';
+$g_smtp_password = 'serverkeyhere';
+$g_smtp_connection_mode = 'tls';
+$g_window_title = 'PlumHQ Bug Tracker';
